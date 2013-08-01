938bca4b2e070eadde63953f05e20979feb52c1a
Andrew Vos <andrew.vos@gmail.com>
2013-08-01 01:32:47 +0100

    switch to redigo and get it working with heroku

--- main.go
+++ main.go
@@ -7,8 +7,9 @@ import (
   "time"
   "strconv"
   "net"
-  "github.com/fzzy/radix/redis"
+  "github.com/garyburd/redigo/redis"
   "github.com/hoisie/web"
+  "github.com/soveran/redisurl"
 )
 
 func main() {
@@ -16,23 +17,42 @@ func main() {
   web.Get("/client/(.*)/views", clientViews)
 
   if os.Getenv("PORT") == "" {
-    web.Run(":9292")
+    web.Run(":8080")
   } else {
     web.Run(":" + os.Getenv("PORT"))
   }
 }
 
-func storeClientHit(clientId string, userId string) {
-  fmt.Println(clientId, userId)
-  connection, err := redis.DialTimeout("tcp", "127.0.0.1:6379", time.Duration(10)*time.Second)
-  if err == nil {
-    now := time.Now().Unix()
-    connection.Cmd("zrem", clientId, userId)
-    connection.Cmd("zadd", clientId, now, userId)
-  } else {
-    fmt.Println(err)
+func getConnection() redis.Conn {
+  if os.Getenv("REDISCLOUD_URL") != "" {
+    os.Setenv("REDIS_URL", os.Getenv("REDISCLOUD_URL"))
   }
+
+  if redisUrl := os.Getenv("REDIS_URL"); redisUrl != "" {
+    connection, err := redisurl.Connect()
+    if err != nil {
+      fmt.Println(err)
+      return nil
+    }
+    return connection
+  } else {
+    connection, err := redis.Dial("tcp", ":6379")
+    if err != nil {
+      fmt.Println(err)
+      return nil
+    }
+    return connection
+  }
+}
+
+func storeClientHit(clientId string, userId string) {
+  connection := getConnection()
   defer connection.Close()
+  if connection != nil {
+    now := time.Now().Unix()
+    connection.Do("ZREM", clientId, userId)
+    connection.Do("ZADD", clientId, now, userId)
+  }
 }
 
 func tracker(ctx *web.Context, clientId string) {
@@ -49,15 +69,20 @@ func tracker(ctx *web.Context, clientId string) {
 }
 
 func clientViews(clientId string) string {
-  connection, err := redis.DialTimeout("tcp", "127.0.0.1:6379", time.Duration(10)*time.Second)
-  if err == nil {
+  connection := getConnection()
+  defer connection.Close()
+  if connection != nil {
     now := time.Now().Unix()
-    connection.Cmd("zremrangebyscore", clientId, 0, now - 60)
-    result := connection.Cmd("zcount", clientId, "-inf", "+inf")
-    currentCount,_ := result.Int()
-    return `{"views": "` + strconv.Itoa(currentCount) + `"}`
+    connection.Do("ZREMRANGEBYSCORE", clientId, 0, now - 60)
+    result,err := redis.Int(connection.Do("ZCOUNT", clientId, "-inf", "+inf"))
+
+    if err == nil {
+      return `{"views": "` + strconv.Itoa(result) + `"}`
+    } else {
+      fmt.Println(err)
+      return `{"views": "0"}`
+    }
   } else {
-    fmt.Println(err)
     return `{"error": true}`
   }
 }
