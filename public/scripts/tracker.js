99d9e339665902582d35a871dc9803111ecffab9
Andrew Vos <andrew.vos@gmail.com>
2013-08-24 00:47:29 +0100

    encode client id

--- public/scripts/tracker.js
+++ public/scripts/tracker.js
@@ -2,7 +2,7 @@
   var referer = document.referrer;
   // var page = window.location.href;
 	var clientId = document.location.host;
-	var trackerUrl = "https://statistic.li/client/" + clientId + "/tracker.gif?referer=" + encodeURIComponent(referer);
+	var trackerUrl = "https://statistic.li/client/" + encodeURIComponent(clientId) + "/tracker.gif?referer=" + encodeURIComponent(referer);
 	var img     = new Image();
 	img.src = trackerUrl;
 })();
