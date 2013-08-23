7df48e09c522e603461e9b8ec3fc9e230f58e9ea
Andrew Vos <andrew.vos@gmail.com>
2013-08-24 00:46:23 +0100

    use website host as client id

--- public/scripts/tracker.js
+++ public/scripts/tracker.js
@@ -1,6 +1,7 @@
 !(function(){
   var referer = document.referrer;
   // var page = window.location.href;
+	var clientId = document.location.host;
 	var trackerUrl = "https://statistic.li/client/" + clientId + "/tracker.gif?referer=" + encodeURIComponent(referer);
 	var img     = new Image();
 	img.src = trackerUrl;
