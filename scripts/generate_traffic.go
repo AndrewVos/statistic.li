4398b1c93d121663548ef8d952e3a0aa9afa0bb6
Andrew Vos <andrew.vos@gmail.com>
2013-08-24 22:02:32 +0100

    add script to generate some traffic

---
+++ scripts/generate_traffic.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"math/rand"
+	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
+)
+
+func hitTracker(clientId string, page string, referer string, cookies []*http.Cookie) []*http.Cookie {
+	client := &http.Client{}
+
+	page = url.QueryEscape(page)
+	referer = url.QueryEscape(referer)
+	request, _ := http.NewRequest("GET", "http://localhost:8080/client/"+clientId+"/tracker.gif?page="+page+"&referer="+referer, nil)
+	if cookies != nil {
+		for _, cookie := range cookies {
+			request.AddCookie(cookie)
+		}
+	}
+	response, _ := client.Do(request)
+	response.Body.Close()
+	return response.Cookies()
+}
+
+func randomPage() string {
+	number := rand.Intn(100)
+	return "http://client.com/pages/page" + strconv.Itoa(number) + ".html"
+}
+
+func randomlyHitTracker(clientId string) {
+	cookies := hitTracker("client.com", randomPage(), "google.co.uk", nil)
+	for i := 0; i < rand.Intn(10000); i++ {
+		hitTracker("client.com", randomPage(), "google.co.uk", cookies)
+	}
+}
+
+func main() {
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func() {
+			randomlyHitTracker("client.com")
+			defer wg.Done()
+		}()
+	}
+	wg.Wait()
+}
