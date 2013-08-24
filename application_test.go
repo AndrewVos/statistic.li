34cbd0f7a0dd3b9504dd9c263626cec54c5cb264
Andrew Vos <andrew.vos@gmail.com>
2013-08-24 02:40:01 +0100

    massive test refactor

--- application_test.go
+++ application_test.go
@@ -16,22 +16,50 @@ func flushDatabase() {
 	session.DB("").C("ClientHits").DropCollection()
 }
 
+func get(url string, cookies []*http.Cookie) (*http.Response, string) {
+	client := &http.Client{}
+	request, _ := http.NewRequest("GET", url, nil)
+	if cookies != nil {
+		for _, cookie := range cookies {
+			request.AddCookie(cookie)
+		}
+	}
+
+	response, _ := client.Do(request)
+	body, _ := ioutil.ReadAll(response.Body)
+	response.Body.Close()
+	return response, string(body)
+}
+
+func expectContentType(t *testing.T, path string, contentType string) {
+	server := httptest.NewServer(http.HandlerFunc(clientHandler))
+	defer server.Close()
+	response, _ := get(server.URL+path, nil)
+	if response.Header["Content-Type"][0] != contentType {
+		t.Errorf("Expected %q to return a Content-Type of %q, not %q\n", path, contentType, response.Header["Content-Type"][0])
+	}
+}
+
+func hitTracker(server *httptest.Server, clientId string, userId string, page string, referer string) {
+	cookies := []*http.Cookie{
+		&http.Cookie{Name: "sts", Value: userId},
+	}
+	get(server.URL+"/client/"+clientId+"/tracker.gif?referer="+url.QueryEscape(referer)+"&page="+url.QueryEscape(page), cookies)
+}
+
+func TestContentTypes(t *testing.T) {
+	expectContentType(t, "/client/CLIENT_ID/uniques", "application/json")
+	expectContentType(t, "/client/CLIENT_ID/referers", "application/json")
+	expectContentType(t, "/client/CLIENT_ID/pages", "application/json")
+	expectContentType(t, "/client/CLIENT_ID/tracker.gif", "image/gif")
+}
+
 func TestTrackerRespondsWithGif(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(clientHandler))
 	defer server.Close()
-	client := &http.Client{}
-	request, _ := http.NewRequest("GET", server.URL+"/client/MY_CLIENT_ID/tracker.gif", nil)
-
-	response, _ := client.Do(request)
-	gif, _ := ioutil.ReadAll(response.Body)
-	if string(gif) != string(tracker_gif()) {
-		t.Errorf("Expected:\n%q\nGot:\n%q\n", string(tracker_gif()), string(gif))
-	}
-	response.Body.Close()
-
-	expectedContentType := "image/gif"
-	if response.Header["Content-Type"][0] != expectedContentType {
-		t.Errorf("Expected a Content-Type of %q, not %q\n", expectedContentType, response.Header["Content-Type"][0])
+	_, gif := get(server.URL+"/client/MY_CLIENT_ID/tracker.gif", nil)
+	if gif != string(tracker_gif()) {
+		t.Errorf("Expected:\n%q\nGot:\n%q\n", string(tracker_gif()), gif)
 	}
 }
 
@@ -40,24 +68,12 @@ func TestUniques(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(clientHandler))
 	defer server.Close()
 
-	ipAddress := func(lastPart int) string { return "0.0.0." + strconv.Itoa(lastPart) }
 	numberOfUsers := 5
 	clientId := "andrewvos.com"
 
 	for userHitCount := 0; userHitCount < numberOfUsers; userHitCount++ {
-		client := &http.Client{}
-		request, _ := http.NewRequest("GET", server.URL+"/client/"+clientId+"/tracker.gif", nil)
-		request.Header.Set("X-Forwarded-For", ipAddress(userHitCount%numberOfUsers))
-
-		response, _ := client.Do(request)
-		client.Do(request)
-		for _, cookie := range response.Cookies() {
-			request.AddCookie(cookie)
-		}
-
-		for requestCount := 0; requestCount < 5; requestCount++ {
-			client.Do(request)
-		}
+		response, _ := get(server.URL+"/client/"+clientId+"/tracker.gif", nil)
+		get(server.URL+"/client/"+clientId+"/tracker.gif", response.Cookies())
 	}
 
 	response, _ := http.Get(server.URL + "/client/" + clientId + "/uniques")
@@ -65,36 +81,11 @@ func TestUniques(t *testing.T) {
 	uniques := string(responseBody)
 	response.Body.Close()
 
-	expectedContentType := "application/json"
-	if response.Header["Content-Type"][0] != expectedContentType {
-		t.Errorf("Expected a Content-Type of %q, not %q\n", expectedContentType, response.Header["Content-Type"][0])
-	}
-
-	expected, _ := json.Marshal(UniquesCount{Count: 10})
+	expected, _ := json.Marshal(UniquesCount{Count: numberOfUsers})
 
 	if uniques != string(expected) {
 		t.Errorf("Expected:\n%q\nGot:\n%q\n", string(expected), uniques)
 		t.Fail()
-	}
-}
-
-func TestReferersContentType(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(clientHandler))
-	defer server.Close()
-	response, _ := http.Get(server.URL + "/client/CLIENT_ID/referers")
-	expectedContentType := "application/json"
-	if response.Header["Content-Type"][0] != expectedContentType {
-		t.Errorf("Expected a Content-Type of %q, not %q\n", expectedContentType, response.Header["Content-Type"][0])
-	}
-}
-
-func TestTopPagesContentType(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(clientHandler))
-	defer server.Close()
-	response, _ := http.Get(server.URL + "/client/CLIENT_ID/pages")
-	expectedContentType := "application/json"
-	if response.Header["Content-Type"][0] != expectedContentType {
-		t.Errorf("Expected a Content-Type of %q, not %q\n", expectedContentType, response.Header["Content-Type"][0])
 	}
 }
 
@@ -103,11 +94,9 @@ func TestEmptyReferers(t *testing.T) {
 	server := httptest.NewServer(http.HandlerFunc(clientHandler))
 	defer server.Close()
 
-	response, _ := http.Get(server.URL + "/client/CLIENT_ID/referers")
-	bytes, _ := ioutil.ReadAll(response.Body)
-	response.Body.Close()
-	if string(bytes) != "[]" {
-		t.Errorf("Expected an empty json array, but got this:\n%q", string(bytes))
+	_, body := get(server.URL+"/client/CLIENT_ID/referers", nil)
+	if body != "[]" {
+		t.Errorf("Expected an empty json array, but got this:\n%q", body)
 	}
 }
 
@@ -144,14 +133,11 @@ func TestReferers(t *testing.T) {
 		}
 	}
 
-	response, _ := http.Get(server.URL + "/client/CLIENT_ID/referers")
-	bytes, _ := ioutil.ReadAll(response.Body)
-	response.Body.Close()
-
+	_, body := get(server.URL+"/client/CLIENT_ID/referers", nil)
 	expected, _ := json.Marshal(hits[:10])
 
-	if string(bytes) != string(expected) {
-		t.Errorf("Expected:\n%q\nGot:\n%q\n", string(expected), string(bytes))
+	if body != string(expected) {
+		t.Errorf("Expected:\n%q\nGot:\n%q\n", string(expected), body)
 		t.Fail()
 	}
 }
@@ -183,26 +169,11 @@ func TestTopPages(t *testing.T) {
 		}
 	}
 
-	response, _ := http.Get(server.URL + "/client/CLIENT_ID/pages")
-	bytes, _ := ioutil.ReadAll(response.Body)
-	response.Body.Close()
-
+	_, body := get(server.URL+"/client/CLIENT_ID/pages", nil)
 	expected, _ := json.Marshal(hits[:10])
 
-	if string(bytes) != string(expected) {
-		t.Errorf("Expected:\n%q\nGot:\n%q\n", string(expected), string(bytes))
+	if body != string(expected) {
+		t.Errorf("Expected:\n%q\nGot:\n%q\n", string(expected), body)
 		t.Fail()
 	}
-}
-
-func hitTracker(server *httptest.Server, clientId string, userId string, page string, referer string) (*http.Response, error) {
-	client := &http.Client{}
-	request, _ := http.NewRequest("GET", server.URL+"/client/"+clientId+"/tracker.gif?referer="+url.QueryEscape(referer)+"&page="+url.QueryEscape(page), nil)
-	request.Header.Set("X-Forwarded-For", "192.134.123.23")
-	request.Header.Set("HTTP_REFERER", referer)
-	request.AddCookie(&http.Cookie{Name: "sts", Value: userId})
-
-	response, err := client.Do(request)
-	response.Body.Close()
-	return response, err
 }
