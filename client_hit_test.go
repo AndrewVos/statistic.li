2b878be87b950eef9fb125466d5d96d90008923e
Andrew Vos <andrew.vos@gmail.com>
2013-08-25 23:54:18 +0100

    unit test for latest client hits

--- client_hit_test.go
+++ client_hit_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"strconv"
 	"testing"
+	"time"
 )
 
 func TestShowsGoogleSearchResults(t *testing.T) {
@@ -32,5 +33,19 @@ func TestUniqueUsers(t *testing.T) {
 	uniques := Uniques("site.com")
 	if uniques.Count != 10 {
 		t.Errorf("Expected unique users to be %v, but was %v", 10, uniques.Count)
+	}
+}
+
+func TestLatestHits(t *testing.T) {
+	DeleteAllClientHits()
+	for userId := 0; userId < 10; userId++ {
+		hit1 := &ClientHit{ClientID: "site.com", UserID: strconv.Itoa(userId)}
+		hit2 := &ClientHit{ClientID: "site.com", UserID: strconv.Itoa(userId)}
+		hit1.Save()
+		hit2.Save()
+		hit2.Date = time.Now().Add(-5 * time.Minute)
+	}
+	if len(LatestClientHits("site.com")) != 10 {
+		t.Errorf("Expected to only see the latest client hits")
 	}
 }
