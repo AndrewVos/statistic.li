6cda7967d7a2e3b7f56ca3897a06842dc5dd953e
Andrew Vos <andrew.vos@gmail.com>
2013-08-26 14:38:54 +0100

    add ipad screenie

--- application.go
+++ application.go
@@ -36,6 +36,7 @@ func Start() {
 	serveFile("/scripts/jquery.sparkline.min.js", "./public/scripts/jquery.sparkline.min.js")
 	serveFile("/scripts/tracker.js", "./public/scripts/tracker.js")
 	serveFile("/styles/bootstrap.min.css", "./public/styles/bootstrap.min.css")
+	serveFile("/images/ipad.png", "./public/images/ipad.png")
 
 	if os.Getenv("PORT") == "" {
 		http.ListenAndServe(":8080", nil)
